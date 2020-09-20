fb8d8d2573434372852b9fda9adfba9e943c0094
mo.sw <mo.sw@ideasoft.io>
2020-09-20 22:25:23 +0300

    collection items and collection preview components ready

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import {Switch} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
-
+import {ShopPage} from './pages/shop/shop.component'
 import HomePage from "./pages/homepage/homepage.component";
+
 
 function App() {
     return (
         <div>
             <Switch>
-                <HomePage/>
+                <Route exact path={'/'} component={HomePage}/>
+                <Route path={'/shop'} component={ShopPage} />
             </Switch>
         </div>
     );
